4fee3cca18282d6d52452529f0ad15dcde766d01
Milan Kodinariya <milankodinariya1995@gmail.com>
2026-03-05 19:27:24 +0530

    Fixed json size issue

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ async function createServer() {
         origin: process.env.FRONTEND_URL,
         credentials: true,
     }));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json({ limit: '50mb' }));
+    app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
     app.use('/api', router);
 
